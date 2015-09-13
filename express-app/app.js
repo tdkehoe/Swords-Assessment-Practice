4dbb86f8f966b6b652c18af88ef2332a50cca420
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-09-13 09:56:52 -0600

    All Express routes working.

--- express-app/app.js
+++ express-app/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api/routes', swords); // matches the routes variable set on line 3
+app.use('/api/swords', swords); // the first argument is the name of the database, the second argument matches the routes variable set on line 3
 
 app.listen(process.env.PORT || 8080);
 console.log('Server started on localhost://8080');
--- express-app/routes/routes.js
+++ express-app/routes/routes.js
@@ -3,4 +3,65 @@ var router = express.Router();
 var db = require('monk')('localhost/swords'); // this is the name of the database, make it semantic
 var Swords = db.get('swords'); // the name of the database
 
+router.get('/', function(req, res) {  // INDEX
+  Swords.find({}, function(err, swords) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(200).json(swords); // OK
+  })
+});
+
+router.post('/', function(req, res) { // CREATE
+  Swords.insert(req.body, function(err, sword) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(201).json(sword); // Created
+  });
+})
+
+router.get('/new', function(req, res){ // NEW
+  // goes to forms page for user to enter a new item
+  // this route isn't needed in Express because it
+  // doesn't get any data from the database
+  // just use a href anchor in Angular to go to the forms page
+});
+
+router.get('/:id', function(req, res) { //SHOW
+  Swords.findOne({_id: req.params.id}, function(err, sword) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(200).json(sword); // OK
+  })
+})
+
+router.get('/:id/edit', function(req, res){ // EDIT (identical to SHOW route)
+  Swords.findOne({_id: req.params.id}, function(err, sword) {
+    if (err) {
+      res.send(err)
+    }
+    res.status(200).json(sword) // OK
+  })
+});
+
+router.put('/:id', function(req, res) { // UPDATE
+  Swords.findAndModify({_id: req.params.id}, req.body, function(err, sword) {
+    if (err) {
+      throw err;
+    }
+    res.json(req.body);
+  })
+})
+
+router.delete('/:id', function(req, res) { //DESTROY
+  Swords.remove({_id: req.params.id}, function(err, sword){
+    if (err) {
+      throw err;
+    }
+    res.status(204).json(sword); // No Content
+  });
+});
+
 module.exports = router;
