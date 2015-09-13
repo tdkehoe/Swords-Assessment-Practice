548b7def25055f575a4a39c9ca1e81891a985ff9
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-09-13 10:51:21 -0600

    Angular sending data to the database.

--- angular-app/javascript/controllers/NewController.js
+++ angular-app/javascript/controllers/NewController.js
@@ -1,3 +1,16 @@
-app.controller('NewController', ['$scope', function($scope) {
-  $scope.message = "Connected";
+app.controller('NewController', ["$scope", '$http', '$location', function($scope, $http, $location){
+  console.log("New controller");
+  $scope.addSword = function(sword){
+    var sword = {
+      swordName:  $scope.sword.swordName,
+      swordOwner: $scope.sword.swordOwner,
+      likes: 0
+    }
+    $http.post('http://localhost:8080/api/swords/', sword).then(function(response) { // NEW
+      console.log("Sword added.");
+      $location.path( "/" );
+    }, function(response) {
+      console.log("Invalid URL");
+    });
+  }
 }]);
