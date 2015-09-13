e120ae4194075b95d8bb949923e5babb3c0ad629
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-09-13 10:56:27 -0600

    Database saving and serving data to Angular.

--- angular-app/javascript/controllers/HomeController.js
+++ angular-app/javascript/controllers/HomeController.js
@@ -1,3 +1,8 @@
-app.controller('HomeController', ['$scope', function($scope) {
-  $scope.message = "Connected";
+app.controller('HomeController', ['$scope', '$http', function($scope, $http){
+  console.log("Home controller.");
+  $http.get('http://localhost:8080/api/swords/').then(function(response) { // INDEX
+    $scope.swords = response.data;
+  }, function(response) {
+    console.log("Invalid URL");
+  });
 }]);
